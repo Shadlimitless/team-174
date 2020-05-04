fdfd088c0dc3d75809f93446c957645b750057d8
paulonteri <onteripaul@gmail.com>
2020-05-04 18:46:53 +0300

    Added the homepage component to the index route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import { Route, Switch, HashRouter } from "react-router-dom";
+import Homepage from "./components/HomePage/HomePage";
+
 function App() {
   return (
     <Provider store={store}>
-      <div></div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </HashRouter>
     </Provider>
   );
 }
---
+++ frontend/src/components/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <p>Welcome to the homepage</p>;
+};
+
+export default HomePage;
